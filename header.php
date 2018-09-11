249839bfa4f2382870992836285c7c23a093e590
piti3 <patrik.ravnak@gmail.com>
2018-09-11 08:58:19 +0200

    V novi datoteki
    Header in footer sem kopiral v novi datoteki.

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Spletni testi</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="subpage">
+
+		<!-- Header -->
+			<header id="header">
+				<nav class="left">
+					<a href="#menu"><span>Meni</span></a>
+				</nav>
+				<a href="index.html" class="logo">Spletni testi</a>
+				<nav class="right">
+					<a href="#" class="button alt">Prijava</a>
+				</nav>
+			</header>
+
+		<!-- Menu -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.php">Domov</a></li>
+				</ul>
+				<ul class="actions vertical">
+					<li><a href="#" class="button fit">Prijava</a></li>
+				</ul>
+			</nav>
