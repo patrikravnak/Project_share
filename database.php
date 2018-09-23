80fb84c513834f3e03ed270964a76c295f80ea29
piti3 <patrik.ravnak@gmail.com>
2018-09-23 16:02:00 +0200

    Uredil povezavo na podatkovno bazo

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $database = 'project_share';
 $user = 'root';
 $pass = '';
-$charset = 'utf8_slovenian_ci';
+$charset = 'utf8';
 $dsn = "mysql:host=$host;dbname=$database;charset=$charset";
 $opt = [
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -11,7 +11,11 @@ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
-$pdo = new PDO($dsn, $user, $pass, $opt);
+try {
+     $pdo = new PDO($dsn, $user, $pass, $opt);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 
 $salt = "znj192nijnz";
 ?>
